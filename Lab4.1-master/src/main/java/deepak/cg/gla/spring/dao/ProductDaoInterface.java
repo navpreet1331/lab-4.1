41f14682ef05b8a68df58999b98ec34b67f823cd
navpreet1331 <45588234+navpreet1331@users.noreply.github.com>
2020-04-30 22:28:59 +0530

    Add files via upload

---
+++ Lab4.1-master/src/main/java/deepak/cg/gla/spring/dao/ProductDaoInterface.java
@@ -0,0 +1,14 @@
+package deepak.cg.gla.spring.dao;
+
+import java.util.List;
+
+import deepak.cg.gla.spring.entity.Product;
+
+
+public interface ProductDaoInterface {
+	
+	public boolean create(Product product);
+	public List<Product> reterive();
+	
+
+}
---
+++ Lab4.1-master/src/main/java/deepak/cg/gla/spring/service/ProductServiceInterface.java
@@ -0,0 +1,14 @@
+package deepak.cg.gla.spring.service;
+
+import java.util.List;
+
+import deepak.cg.gla.spring.entity.Product;
+
+
+public interface ProductServiceInterface {
+	
+	public Product save(Product product);
+
+	public List<Product> reterive();
+
+}
